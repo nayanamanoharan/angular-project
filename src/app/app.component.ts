dd9157d5c3e4db6b6a5bf20950b857e386d30bed
nayanamanoharan <nayanapm001@gmail.com>
2021-03-02 09:32:23 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+public a=0
+Function () {
+  this.a=this.a+1
+  
+}
+}
